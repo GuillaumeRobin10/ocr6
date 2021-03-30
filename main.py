cacc69c60b638a69bf6ec9ba0b17e7153b65a44b
GuillaumeRobin10 <guillaume.robin10@gmail.com>
2021-03-30 15:23:51 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,28 +1,14 @@
-from flask import Flask, render_template
-from request_api import requesting
+from flask import Flask
+from python.request_api import get_films_info
+from python.setting import HEADERS
 app = Flask(__name__)
 
-HOME ="http://127.0.0.1:8000/api/v1/titles/?format=json&genre="
-end = "&imdb_score_min=8&sort_by=+-imdb_score"
-Theme1 = "Romance"
-Theme2 = "Action"
-Theme3 = "Drama"
+
+@app.route('/notheme')
+def rooting1():
+    return get_films_info(),HEADERS
 
 
-@app.route('/')
-def main():
-    best_film = requesting(HOME + end, True, True)
-    best_films = requesting(HOME + end, True)
-    theme1_film = requesting(HOME + Theme1 + end)
-    theme2_film = requesting(HOME + Theme2 + end)
-    theme3_film = requesting(HOME + Theme3 + end)
-    return render_template("index.html",
-                           best_film=best_film,
-                           best_films=best_films,
-                           theme1_film=theme1_film,
-                           theme2_film=theme2_film,
-                           theme3_film=theme3_film,
-                           theme1=Theme1,
-                           theme2=Theme2,
-                           theme3=Theme3
-                           )
+@app.route('/<genre>')
+def rooting(genre):
+    return get_films_info(genre),HEADERS
