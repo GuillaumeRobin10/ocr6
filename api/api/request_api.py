90fbab4a7ba3d70203b25e0447c53f3b502c3140
GuillaumeRobin10 <guillaume.robin10@gmail.com>
2021-03-31 11:56:14 +0200

    Add files via upload

---
+++ api/api/request_api.py
@@ -0,0 +1,35 @@
+import requests
+from api.setting import HOME,SETTING
+
+
+def requesting_api(genre, increment=1):
+    SETTING.update({"genre": genre,"page": increment})
+    response = requests.get(HOME, params=SETTING)
+    if response.ok:
+        return response.json()
+    else:
+        print("can't reach api")
+
+
+def get_film_url(films, response):
+    for film in response["results"]:
+        films.append(film["url"])
+    return films
+
+
+def get_best_film_of_a_category(genre):
+    films = []
+    for increment in range(1, 3):
+        response = requesting_api(genre, increment)
+        films = get_film_url(films, response)
+    return films
+
+
+def get_films_info(genre=""):
+    films = get_best_film_of_a_category(genre)
+    json_films = {}
+    i = 1
+    for film in films:
+        json_films.update({f"film{i}": requests.get(film).json()})
+        i += 1
+    return json_films
